f5ddd7085ca00d7ee79c299fd092d6359f754f05
dcaban <davidjcaban@gmail.com>
2017-08-26 09:29:01 -0400

    almost there kinda

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,11 +24,38 @@ $('#qAndA').hide();
         }, 1000);
     }
 
+
+
+
+
+
+
     $('#timer').click(function () {
         $('#timer').text("2:00");
         countdown();
         $('#qAndA').show()
+
     });
+
+    function submitAnswers(){
+        var totalQuestions = 7;
+        var score = 0;
+        var q1 = document.forms["quizForm"]["option1"].value;
+        var q2 = document.forms["quizForm"]["option2"].value;
+        var q3 = document.forms["quizForm"]["option3"].value;
+        var q4 = document.forms["quizForm"]["option4"].value;
+        var q5 = document.forms["quizForm"]["option5"].value;
+        var q6 = document.forms["quizForm"]["option6"].value;
+        var q7 = document.forms["quizForm"]["option7"].value;
+        alert(q1);
+        alert(q2);
+        alert(q3);
+        alert(q4);
+        alert(q5);
+        alert(q6);
+        alert(q7)
+    }
+
 });
 
 
